ff4b6fd5374d3fb90ef08310f898723c83309909
Saman-Safaei <xsamansafaeix@gmail.com>
2023-05-04 16:17:44 +0430

    fix: import only core swiper styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Layout from "@/components/modules/Layout";
-import 'swiper/swiper-bundle.css';
+import 'swiper/swiper.css';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
