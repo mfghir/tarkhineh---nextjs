505267fe7499c52d349b5bbec05f1bf2c50f4097
miss-f-g <fatemeghafari98@gmail.com>
2023-05-04 14:26:37 +0430

    feat: add menu

--- components/templates/Navbar.jsx
+++ components/templates/Navbar.jsx
@@ -1,25 +1,72 @@
-import { HambergerMenu, ShoppingCart, User } from "iconsax-react";
+import {
+  ArrowDown2,
+  HambergerMenu,
+  SearchNormal1,
+  ShoppingCart,
+  User,
+} from "iconsax-react";
 import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
   return (
-    <section className="w-full flex justify-between items-center flex-nowrap p-5 text-Primary">
-      <HambergerMenu />
+    <section className="w-full flex justify-between items-center flex-nowrap p-5">
+      <HambergerMenu className="lg:hidden md:text-2xl" />
 
       <Image
-        src="https://i.postimg.cc/WpJkBChc/Logo-phone.png"
+        className="lg:hidden"
+        src="/img/Logo-phone.png"
         alt="Logo-phone"
         width={102}
         height={32}
       />
+      <Image
+        className="hidden lg:block"
+        src="/img/Logo-desktop.png"
+        alt="Logo-desktop"
+        width={155}
+        height={51}
+      />
 
-      <ul className="flex">
-        <li className="p-1 rounded bg-Tint-1">
-          <ShoppingCart  size="16"/>
-        </li>
+      <ul className="hidden lg:flex justify-between items-center">
         <li>
-          <User  size="16" />
+          <h5 className="text-primary">صفحه اصلی</h5>
+        </li>
+
+        <li className="body-xl text-gray-700 flex justify-center items-center mr-6">
+          شعبه
+          <ArrowDown2 size="16" className="mr-1" />
+        </li>
+
+        <li className="body-xl text-gray-700 flex justify-center items-center mr-6">
+          منو
+          <ArrowDown2 size="16" className="mr-1" />
+        </li>
+
+        <li className="body-xl text-gray-700 flex justify-center items-center mr-6">
+          اعطای نمایندگی
+        </li>
+
+        <li className="body-xl text-gray-700 flex justify-center items-center mr-6">
+          درباره ما
+        </li>
+
+        <li className="body-xl text-gray-700 flex justify-center items-center mr-6">
+          تماس با ما
+        </li>
+      </ul>
+
+      
+
+      <ul className="flex text-primary">
+        <li className="hidden md:flex justify-center items-center p-1 md:p-2 rounded bg-tint-100">
+          <SearchNormal1  />
+        </li>
+        <li className="flex justify-center items-center p-1 md:p-2 rounded bg-tint-100 ml-1 md:mx-2">
+          <ShoppingCart className="w-4 h-4 md:w-6 md:h-6" />
+        </li>
+        <li className="flex justify-center items-center p-1 md:p-2 rounded bg-tint-100 ">
+          <User className="w-4 h-4 md:w-6 md:h-6" />
         </li>
       </ul>
     </section>
