ac1ea7889011319a0c682ea5c978febafa277ea1
Saman-Safaei <xsamansafaeix@gmail.com>
2023-06-09 22:11:53 +0330

    feat: wrap whole app with redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
-import Layout from "@/components/modules/Layout";
+import Layout from '@/components/modules/Layout';
 import 'swiper/swiper.css';
 import '@/styles/globals.css';
+import createStore from '@/store';
+import { Provider } from 'react-redux';
 
 export default function App({ Component, pageProps }) {
+  const [store] = createStore();
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
