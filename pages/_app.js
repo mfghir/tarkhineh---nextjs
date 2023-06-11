618e45ec73afdad3f3e12f489e6785e22d9ec915
saman <xsamansafaeix@gmail.com>
2023-06-11 10:06:13 -0400

    fix: make store persist on page change

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,10 @@ import 'swiper/swiper.css';
 import '@/styles/globals.css';
 import createStore from '@/store';
 import { Provider } from 'react-redux';
+import {useState} from "react";
 
 export default function App({ Component, pageProps }) {
-  const store = createStore();
+  const [store] = useState(() => createStore());
 
   return (
     <Provider store={store}>
