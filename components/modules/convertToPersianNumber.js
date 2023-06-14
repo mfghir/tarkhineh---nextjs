70af61439fb26f39990613c3542271359cb34555
saman <xsamansafaeix@gmail.com>
2023-06-14 12:02:32 +0330

    refactor: remove unused codes

--- components/modules/convertToPersianNumber.js
+++ components/modules/convertToPersianNumber.js
@@ -1,10 +1,4 @@
 export default function convertToPersianNumber(number) {
-    let formattedNumber = ""
-
-
     const persianDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
-    formattedNumber = number?.toLocaleString().replace(/\d/g, digit => persianDigits[digit]);
-
-
-    return formattedNumber;
+    return  number?.toLocaleString().replace(/\d/g, digit => persianDigits[digit]);;
 }
