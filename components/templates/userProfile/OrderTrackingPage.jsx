cfe2230c6df706b6c83392e3d455e57b1d798cb4
mfghir <fatemeghafari77@gmail.com>
2023-06-26 03:44:00 -0700

    feat: add orderTracking data

--- components/templates/userProfile/OrderTrackingPage.jsx
+++ components/templates/userProfile/OrderTrackingPage.jsx
@@ -2,20 +2,38 @@ import { useRouter } from 'next/router';
 import ProfileMenu from './ProfileMenu';
 import {
   ArrowRight2,
+  Box,
   Calendar,
   Clock,
   CloseCircle,
+  Home,
   Location,
+  TickCircle,
   TickSquare,
   Wallet2,
 } from 'iconsax-react';
 import { useSelector } from 'react-redux';
 import Link from 'next/link';
+import orderTrackingData from '@/db/orderTrackingData';
+import Image from 'next/image';
+import { convertToFaNumber } from '@/components/modules/FarsiNumber';
+import { useState } from 'react';
 
 const OrderTrackingPage = () => {
   const { push } = useRouter();
-
   const cart = useSelector(state => state.cart.items);
+
+  const currentOrder = orderTrackingData.filter(
+    food => food.type === 'current'
+  );
+  const canceledOrder = orderTrackingData.filter(
+    food => food.type === 'canceled'
+  );
+  const deliveredOrder = orderTrackingData.filter(
+    food => food.type === 'delivered'
+  );
+
+  const [seeMore, setSeeMore] = useState(false);
 
   return (
     <section className='px-5 lg:px-20 py-2 lg:py-12 lg:flex lg:flex-row lg:justify-between min-h-screen '>
@@ -66,7 +84,7 @@ const OrderTrackingPage = () => {
               </li>
             </ul>
 
-            <div className='w-full py-2 px-3 lg:p-6 mt-6 lg:mt-8 border border-gray-400 rounded'>
+            <div className='w-full h-fit py-2 px-3 lg:p-6 mt-6 lg:mt-8 border border-gray-400 rounded'>
               <div className='flex justify-between items-center mb-4 lg:mb-3'>
                 <p className='caption-md lg:body-sm text-gray-600'>
                   شعبه اقدسیه
@@ -82,34 +100,91 @@ const OrderTrackingPage = () => {
                 </p>
               </div>
 
-              <ul className='flex flex-col items-start text-gray-700'>
-                <li className='flex justify-between items-center'>
+              <ul className='w-full flex flex-col items-start text-gray-700 caption-sm'>
+                <li className='w-full flex justify-between items-center'>
                   <p className='flex justify-center items-center'>
-                    <Calendar />
-                    <span className=''>شنبه، ۸ مرداد، ساعت ۱۸:۵۳</span>
-
+                    <Calendar className='w-3 h-3 lg:w-4 lg:h-4 ml-1' />
+                    <span>شنبه، ۸ مرداد، ساعت ۱۸:۵۳</span>
+                    <span className='mx-1'>مبلغ: ۲۲۸٬۵۰۰ تومان</span>
+                    <span>تخفیف: ۲۸٬۵۰۰ تومان </span>
                   </p>
 
-                  <p className='flex justify-center items-center'>
-                    <Clock />
-                    آماده تحویل تا۲۵:۳۳
+                  <p className='flex justify-center items-center lg:caption-md'>
+                    <Clock size='16' className='ml-1' />
+                    آماده تحویل تا
+                    <span className='mr-1 text-primary'>۲۵:۳۳</span>
                   </p>
                 </li>
 
-                <li className='flex justify-center items-center'>
-                  <Location className='' />
+                <li className='flex justify-center items-center my-2'>
+                  <Location className='w-3 h-3 lg:w-4 lg:h-4 ml-1' />
                   تهران: اقدسیه، بزرگراه ارتش، مجتمع شمیران سنتر، طبقه ۱۰
                 </li>
 
-                <li className='flex justify-center items-center'>
-                  <Wallet2 className='' />
-                  مبلغ:
+                <li className='flex justify-center items-center lg:hidden'>
+                  <Wallet2 className='w-3 h-3 lg:w-4 lg:h-4 ml-1' />
+                  <span className=''>مبلغ: ۲۲۸٬۵۰۰ تومان</span>
+                  <span className='mr-1'>تخفیف: ۲۸٬۵۰۰ تومان </span>
                 </li>
               </ul>
 
-              {cart.map(item => {
-                <div className=''>{item.name}</div>;
-              })}
+              <ul className='w-full flex justify-center items-center my-4 lg:mt-3 text-gray-400'>
+                <li className='flex justify-center items-center text-primary'>
+                  <Home className='' />
+                  <span className='hidden whitespace-nowrap lg:block lg:mr-1 header-6'>
+                    در حال آماده‌سازی
+                  </span>
+                </li>
+
+                <li className='w-full border border-dashed text-gray-400 border-gray-400 mx-1 lg:mx-2'></li>
+
+                <li className='flex justify-center items-center'>
+                  <Box className='w-4 h-4 lg:w-6 lg:h-6' />
+                  <span className='hidden whitespace-nowrap lg:block lg:mr-1 body-sm'>
+                    ارسال توسط پیک
+                  </span>
+                </li>
+
+                <li className='w-full border border-dashed text-gray-400 border-gray-400 mx-1 lg:mx-2'></li>
+
+                <li className='flex justify-center items-center'>
+                  <TickCircle className='w-4 h-4 lg:w-6 lg:h-6' />
+                  <span className='hidden whitespace-nowrap lg:block lg:mr-1 body-sm'>
+                    تحویل سفارش
+                  </span>
+                </li>
+              </ul>
+
+              <ul className='bg-red-200 flex justify-center items-center gap-x-2'>
+                
+              
+                {currentOrder.slice(0, seeMore ? currentOrder.length : 3).map(item => (
+                  <div
+                    key={item.id}
+                    className=' rounded-lg flex flex-col items-center justify-center'>
+                    <div className='relative w-full'>
+                      <Image
+                        src={item.img}
+                        alt={item.name}
+                        fill
+                        className='w-full h-[48px] lg:h-[81px]'
+                      />
+                      <span className='absolute bottom-2 left-2'>
+                        {convertToFaNumber(item.num)}
+                      </span>
+                    </div>
+                    <p className='caption-sm'>{item.name}</p>
+                    <p className='caption-sm'>
+                      {convertToFaNumber(item.price)}تومان
+                    </p>
+                  </div>
+                ))}
+
+
+              </ul>
+
+<button className="caption-sm text-gray-700 mt-2" onClick={()=> setSeeMore(!seeMore) }>مشاهده همه سفارشات</button>
+
             </div>
           </section>
         )}
---
+++ db/orderTrackingData.js
@@ -0,0 +1,146 @@
+const orderTrackingData = [
+  {
+    id: 1,
+    type: 'current',
+    img: '/images/iranianFood/iranianFood1.png',
+    name: 'کوفته برنجی',
+    discountPrice: '180000',
+    price: '145000',
+    num: 5,
+  },
+
+  {
+    id: 2,
+    type: 'current',
+    img: '/images/so1.png',
+    name: 'دلمه برگ کلم',
+    discountPrice: '220000',
+    price: '209000',
+    num: 3,
+  },
+
+  {
+    id: 3,
+    type: 'current',
+    img: '/images/so2.png',
+    name: 'بادمجان شکم‌پر',
+    discountPrice: '150000',
+    price: '136000',
+    num: 4,
+  },
+
+  {
+    id: 4,
+    type: 'current',
+    img: '/images/so3.png',
+    name: 'کالزونه اسفناج',
+    discountPrice: '190000',
+    price: '177000',
+    num: 2,
+  },
+  //
+  //
+  //
+  //
+
+  {
+    id: 11,
+    type: 'canceled',
+    img: '/images/sandwich/sandwich4.png',
+    name: 'پنینی اسفناج',
+    discountPrice: '210000',
+    price: '190000',
+    num: 4,
+  },
+  {
+    id: 12,
+    type: 'canceled',
+    img: '/images/sandwich/sandwich3.png',
+    name: 'ساندویچ کتلت کدو سبز',
+    ingredient: 'کدو سبز، هویج، سیب زمینی، پیاز',
+    discountPrice: '',
+    price: '145000',
+    num: 5,
+  },
+  {
+    id: 13,
+    type: 'pizza',
+    img: '/images/pizza/pizza8.png',
+    name: 'پیتزا پنیر',
+    discountPrice: '125000',
+    price: '105000 ',
+    num: 7,
+  },
+  {
+    id: 14,
+    type: 'pizza',
+    img: '/images/pizza/pizza4.png',
+    name: 'پیتزا قارچ',
+    discountPrice: '215000',
+    price: '175000',
+    num: 3,
+  },
+  {
+    id: 15,
+    type: 'pizza',
+    img: '/images/pizza/pizza5.png',
+    name: 'پیتزا پپرونی',
+    discountPrice: '',
+    price: '100000',
+    num: '4',
+  },
+  //
+  //
+  //
+  //
+  //
+  //
+
+  {
+    id: 21,
+    type: 'delivered',
+    img: '/images/iranianFood/iranianFood5.png',
+    name: 'فلافل',
+    discountPrice: '',
+    price: '80000',
+    num: 2,
+  },
+  {
+    id: 22,
+    type: 'delivered',
+    img: '/images/iranianFood/iranianFood6.png',
+    name: 'کله جوش',
+    discountPrice: '210000',
+    price: '203000',
+    num: 6,
+  },
+  {
+    id: 23,
+    type: 'delivered',
+    img: '/images/iranianFood/iranianFood7.png',
+    name: 'بورانی بادمجان',
+    discountPrice: '170000',
+    price: '148000',
+    num: 4,
+  },
+  {
+    id: 24,
+    type: 'delivered',
+    img: '/images/iranianFood/iranianFood8.png',
+    name: 'بادمجان شکم‌پر',
+    discountPrice: '150000',
+    price: '136000',
+    num: 5,
+  },
+  {
+    id: 25,
+    type: 'delivered',
+    img: '/images/iranianFood/iranianFood9.png',
+    name: 'دلمه برگ کلم',
+    discountPrice: '220000',
+    price: '209000',
+    num: 4,
+  },
+];
+
+export default orderTrackingData;
