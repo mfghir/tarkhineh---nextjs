f7f1df13468701be2e6869ddcdc812fdd8232813
mfghir <fatemeghafari77@gmail.com>
2023-08-02 03:16:34 -0700

    feat: add second modal in modalAddress

--- components/modules/ModalAddress.js
+++ components/modules/ModalAddress.js
@@ -1,11 +1,34 @@
 import { useState } from 'react';
 import Map from './Map/index';
+import { useSelector } from 'react-redux';
+import { Input } from '@material-tailwind/react';
 
 const ModalAddress = () => {
+  const addressValue = useSelector(state => state.input.addressValue);
   const [showModal, setShowModal] = useState(false);
   const toggleModal = () => {
     setShowModal(!showModal);
   };
+
+  const [touched, setTouched] = useState({});
+  const [inpVal, setInpVal] = useState({
+    addressTitle: '',
+    phone: '',
+    receiverName: '',
+    receiverPhone: '',
+    addressDetail: '',
+  });
+
+  const changeHandler = e => {
+    setInpVal({ ...inpVal, [e.target.name]: e.target.value });
+    console.log(inpVal);
+  };
+
+  const touchedHandler = e => {
+    setTouched({ ...touched, [e.target.name]: true });
+  };
+
+  const handleSubmit = e => {};
 
   return (
     <section
@@ -16,46 +39,109 @@ const ModalAddress = () => {
         className='absolute w-full h-full inset-0 exitPage-bg'
         onClick={toggleModal}></div>
 
-      <div className='w-full lg:w-96 bg-white rounded-lg overflow-hidden transform transition-all mx-5'>
-        <div className='bg-gray-300 flex justify-end items-center py-4 px-6'>
-          <p className='caption-lg lg:header-7 text-gray-800 text-center mx-auto justify-start'>
-            افزودن آدرس
-          </p>
+      {addressValue === '' ? (
+        <div className='w-full lg:w-96 bg-white rounded-lg overflow-hidden transform transition-all mx-5'>
+          <div className='bg-gray-300 flex justify-end items-center py-4 px-6'>
+            <p className='caption-lg lg:header-7 text-gray-800 text-center mx-auto justify-start'>
+              افزودن آدرس
+            </p>
 
-          <button onClick={toggleModal}>
-            <svg
-              className='h-6 w-6 text-gray-700 cursor-pointer'
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'>
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M6 18L18 6M6 6l12 12'
-              />
-            </svg>
-          </button>
+            <button onClick={toggleModal}>
+              <svg
+                className='h-6 w-6 text-gray-700 cursor-pointer'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'>
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M6 18L18 6M6 6l12 12'
+                />
+              </svg>
+            </button>
+          </div>
+
+          <div className='w-full flex flex-col'>
+            <span className='w-full h-80 overflow-hidden relative'>
+              <Map />
+            </span>
+          </div>
         </div>
+      ) : (
+        ''
+      )}
 
-        <div className='w-full flex flex-col'>
-          <span className='w-full h-60 overflow-hidden relative'>
-            <Map />
+      {addressValue === '' ? (
+        ''
+      ) : (
+        <div className='w-full lg:w-96 bg-white rounded-lg overflow-hidden transform transition-all mx-5'>
+          <div className='bg-gray-300 flex justify-end items-center py-4 px-6'>
+            <p className='caption-lg lg:header-7 text-gray-800 text-center mx-auto justify-start'>
+              ثبت آدرس
+            </p>
 
-            {/*            
-          <iframe
-            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d207344.07543142117!2d51.18284130717154!3d35.70774021876205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e00491ff3dcd9%3A0xf0b3697c567024bc!2sTehran%2C%20Tehran%20Province%2C%20Iran!5e0!3m2!1sen!2s!4v1687255819474!5m2!1sen!2s'
-            // width='246'
-            // height='205'
-            style={{ border: '0' , width: '100%', height:'100%' }}
-            allowFullScreen=''
-            loading='lazy'
-            referrerPolicy='no-referrer-when-downgrade'></iframe> */}
-          </span>
-         
+            <button onClick={toggleModal}>
+              <svg
+                className='h-6 w-6 text-gray-700 cursor-pointer'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'>
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth={2}
+                  d='M6 18L18 6M6 6l12 12'
+                />
+              </svg>
+            </button>
+          </div>
+
+          <div className='w-full h-full min-h-[200px] flex flex-col p-6'>
+            {/* <Input dir='rtl' label="عنوان آدرس" color='#417F56' /> */}
+
+            {/* <div className='.form-container'> */}
+            <form className='form-container flex flex-col '>
+              <div class='relative mt-2 rtl'>
+                <input
+                  className='block w-full border-gray-400 rounded-md focus:ring focus:ring-gray-800 focus:border-gray-800 body-sm'
+                  type='text'
+                  name='addressTitle'
+                  required
+                  value={inpVal.addressTitle}
+                  onChange={changeHandler}
+                  onFocus={touchedHandler}
+                />
+                <label
+                  htmlFor='addressTitle'
+                  className='body-sm caption-md absolute left-0 -top-2 transition-all duration-4 ease-in-out'>
+                  عنوان آدرس
+                </label>
+              </div>
+
+              <div class='relative mt-2'>
+                <input
+                  className='block w-full border-gray-400 rounded-md focus:ring focus:ring-gray-800 focus:border-gray-800 body-sm'
+                  type='text'
+                  name='phone'
+                  required
+                  value={inpVal.phone}
+                  onChange={changeHandler}
+                  onFocus={touchedHandler}
+                />
+                <label
+                  htmlFor='phone'
+                  className='body-sm caption-md absolute left-0 -top-2 transition-all duration-300 ease-in-out'>
+                  شماره همراه
+                </label>
+              </div>
+            </form>
+            {/* </div> */}
+          </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
--- components/templates/userProfile/AddressPage.jsx
+++ components/templates/userProfile/AddressPage.jsx
@@ -1,4 +1,4 @@
-import { ArrowRight2 } from 'iconsax-react';
+import { AddCircle, ArrowRight2 } from 'iconsax-react';
 import ProfileMenu from './ProfileMenu';
 import { useRouter } from 'next/router';
 
@@ -10,7 +10,6 @@ const AddressPage = () => {
   const { push } = useRouter();
   const [openModalAdress, setOpenModalAdress] = useState(false);
   const addressValue = useSelector(state => state.input.addressValue);
-
 
   return (
     <section className='px-5 lg:px-20 py-2 lg:py-12 lg:flex lg:flex-row lg:justify-between min-h-screen lg:min-h-fit'>
@@ -25,32 +24,35 @@ const AddressPage = () => {
             size='16'
             onClick={() => push('/profile')}
           />
-          <p className='w-full header-7 lg:header-6 text-gray-800 text-center mx-auto lg:text-right lg:pb-2 lg:border-b lg:border-gray-400'>
+          <p className='w-full header-7 lg:header-6 text-gray-800 text-center mx-auto lg:text-right lg:pb-2 lg:border-b lg:border-gray-400 lg:flex lg:justify-between  '>
+            {/* <p className='w-full header-7 lg:header-6 text-gray-800 text-center mx-auto'></p> */}
             آدرس ها
-          </p>
 
-          <p className="bg-red-200">
-            -{addressValue ? "jdfdjj" : ""}
-          </p>
 
+            {addressValue === '' ? "" : (
+              <p className='hidden lg:flex justify-center items-center text-primary '>
+                <AddCircle size='16' />
+                <span className='caption-md whitespace-nowrap mr-1'>
+                  افزودن آدرس جدید
+                </span>
+              </p>
+            )}
+          </p>
         </div>
 
         <div className='bg-[url("/images/Empty-page.png")] bg-center bg-no-repeat min-h-[200px] lg:min-h-0 border border-gray-400 rounded-lg lg:border-none p-6 mt-6 flex justify-center items-center flex-col lg:mt-12'>
           <p className='caption-sm lg:body-xl text-gray-700 text-center'>
             شما در حال حاضر هیچ آدرسی ثبت نکرده‌اید!
           </p>
-          <button className='caption-sm lg:button-lg rounded border border-primary text-primary hover:text-shade-200 hover:border-shade-200 active:text-shade-300 active:border-shade-300 duration-300 py-1 px-4 lg:px-12  mt-4 lg:mt-8'
-          onClick={()=> setOpenModalAdress(!openModalAdress)}
-          >
+          <button
+            className='caption-sm lg:button-lg rounded border border-primary text-primary hover:text-shade-200 hover:border-shade-200 active:text-shade-300 active:border-shade-300 duration-300 py-1 px-4 lg:px-12  mt-4 lg:mt-8'
+            onClick={() => setOpenModalAdress(!openModalAdress)}>
             افزودن آدرس
           </button>
         </div>
       </section>
 
-
-
-{openModalAdress ? <ModalAddress  /> :"" }
-
+      {openModalAdress ? <ModalAddress /> : ''}
     </section>
   );
 };
